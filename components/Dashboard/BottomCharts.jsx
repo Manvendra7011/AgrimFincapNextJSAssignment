f5c07ddbf26179c440b2c730025f41119ae5492b
Manvendra7011 <manvendrapant7011@gmail.com>
2025-12-23 14:18:56 +0530

    hover effect added

--- components/Dashboard/BottomCharts.jsx
+++ components/Dashboard/BottomCharts.jsx
@@ -7,7 +7,9 @@ export default function BottomCharts() {
       
 
       <div className="bg-white rounded-2xl p-5">
-        <h3 className="text-[13px] mb-4 text-black font-bold">
+        <h3 className="text-[13px] mb-4 text-black font-bold
+               transition-transform duration-200 ease-out
+               hover:translate-x-[4px]">
           Traffic by Device
         </h3>
         <TrafficByDeviceChart />
@@ -18,11 +20,17 @@ export default function BottomCharts() {
   
 
   <div>
-    <h3 className="text-[13px] mb-4 text-black font-bold">
-      Traffic by Location
-    </h3>
-    <TrafficByLocationChart />
-  </div>
+  <h3
+    className="text-[13px] mb-4 text-black font-bold
+               transition-transform duration-200 ease-out
+               hover:translate-x-[4px]"
+  >
+    Traffic by Location
+  </h3>
+
+  <TrafficByLocationChart />
+</div>
+
 
 
   <div className="text-[12px] space-y-2 text-black">
--- components/Dashboard/HeaderRow.jsx
+++ components/Dashboard/HeaderRow.jsx
@@ -1,7 +1,8 @@
 export default function HeaderRow() {
   return (
     <div className="flex items-center justify-between">
-      <h1 className="text-[14px] font-bold text-black">Overview</h1>
+      <h1 className="text-[14px] font-bold text-black transition-transform duration-200 ease-out
+               hover:translate-x-[4px]">Overview</h1>
 
       <div className="flex items-center gap-1 text-[12px] text-black/60 cursor-pointer">
         Today
