705732ca9134dc4ac66e074e90d6582af37da88e
Manvendra7011 <manvendrapant7011@gmail.com>
2025-12-23 14:02:08 +0530

    Minor updates

--- components/Dashboard/HeaderRow.jsx
+++ components/Dashboard/HeaderRow.jsx
@@ -1,7 +1,7 @@
 export default function HeaderRow() {
   return (
     <div className="flex items-center justify-between">
-      <h1 className="text-[14px] font-medium text-black">Overview</h1>
+      <h1 className="text-[14px] font-bold text-black">Overview</h1>
 
       <div className="flex items-center gap-1 text-[12px] text-black/60 cursor-pointer">
         Today
